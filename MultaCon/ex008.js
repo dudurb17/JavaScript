0ae57d333e791ce023d98474c9aca10b7406880f
Eduardo <114515828+dudurb17@users.noreply.github.com>
2023-05-10 00:04:14 -0300

    Multa retrô
    Aparece no console log se você levou multa ou não

---
+++ MultaCon/ex008.js
@@ -0,0 +1,6 @@
+var vel=50.8
+console.log(`A velocide  é: ${vel} Km/h`)
+if(vel>60){//condição simples
+    console.log('Você ultrapassou a velocidade, MULTADO!')
+}
+console.log(`Se beber não dirija`)
